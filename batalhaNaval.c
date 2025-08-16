43f689d8060b4a50cb310ece39a54999deb94d2f
Leticia19972005 <leticiabarros2005.2.2@gmail.com>
2025-08-16 16:16:52 +0000

    Barcos horizontais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,13 +15,21 @@ int main() {
         {0,0,0,0,0,0,0,0,0,0},
     };
 
-    tabuleiro [2][4] = 3;
-    tabuleiro [2][5] = 3;
-    tabuleiro [2][6] = 3; //navio 1 horizontal
+    tabuleiro [1][4] = 3;
+    tabuleiro [1][5] = 3;
+    tabuleiro [1][6] = 3; //navio 1 horizontal
 
     tabuleiro [5][4] = 3;
     tabuleiro [6][4] = 3;
     tabuleiro [7][4] = 3; // navio 2 vertical
+
+    tabuleiro [8] [4] = 3;
+    tabuleiro [9] [3] = 3;
+    tabuleiro [7] [5] = 3; // navio 3 horizontal
+
+    tabuleiro [1] [7] = 3;
+    tabuleiro [2] [6] = 3;
+    tabuleiro [3] [5] = 3; // navio 4 horizontal
 
     int i,j;
     
